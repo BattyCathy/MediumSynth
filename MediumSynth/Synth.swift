0f9fdb2eb20e279f093cf2bf6c7114a47a50d7fc
BattyCathy <austinroach@gmail.com>
2020-11-01 14:31:13 -0800

    First working build of medium touch synth

--- MediumSynth/Synth.swift
+++ MediumSynth/Synth.swift
@@ -5,12 +5,13 @@
 //  Created by Austin Roach on 10/29/20.
 //
 import AVFoundation
-import Foundation
+
 
 
 class Synth {
    
     //MARK: Properties
+    
     public static let shared = Synth()
     
     public var volume: Float {
@@ -18,28 +19,86 @@ class Synth {
             audioEngine.mainMixerNode.outputVolume = newValue
         }
         get {
-            return audioEngine.mainMixerNode.outputVolume
+            audioEngine.mainMixerNode.outputVolume
         }
     }
+    public var frequencyRampValue: Float = 0
+    
+    public var frequency: Float = 440 {
+        didSet {
+            if oldValue != 0 {
+                frequencyRampValue = frequency - oldValue
+            } else {
+                frequencyRampValue = 0
+            }
+        }
+    }
+   
     private var audioEngine: AVAudioEngine
+    private lazy var sourceNode = AVAudioSourceNode { (_, _, frameCount, audioBufferList) -> OSStatus in
+        let ablPointer = UnsafeMutableAudioBufferListPointer(audioBufferList)
+        
+        let localRampValue = self.frequencyRampValue
+        let localFrequency = self.frequency - localRampValue
+        
+        let period = 1 / localFrequency
+        
+        for frame in 0..<Int(frameCount) {
+            let percentComplete = self.time / period
+            let sampleVal = self.signal(localFrequency + localRampValue * percentComplete, self.time)
+            self.time += self.deltaTime
+            self.time = fmod(self.time, period)
+            for buffer in ablPointer {
+                let buf: UnsafeMutableBufferPointer<Float> = UnsafeMutableBufferPointer(buffer)
+                buf[frame] = sampleVal
+            }
+        }
+        self.frequencyRampValue = 0
+        
+        return noErr
+    }
+    
     private var time: Float = 0
     private let sampleRate: Double
     private let deltaTime: Float
     
+
+    
+    private var signal: Signal
     //MARK: Init
-    init() {
+    init(signal: @escaping Signal = Oscillator.sine) {
         audioEngine = AVAudioEngine()
         
+
         
         let mainMixer = audioEngine.mainMixerNode
-        let outputNode = audioEngine.mainMixerNode
+        let outputNode = audioEngine.outputNode
         let format = outputNode.inputFormat(forBus: 0)
         
         sampleRate = format.sampleRate
         deltaTime = 1 / Float(sampleRate)
+        
+        self.signal = signal
+        
+        let inputFormat = AVAudioFormat(commonFormat: format.commonFormat,
+                                        sampleRate: sampleRate,
+                                        channels: 1,
+                                        interleaved: format.isInterleaved)
+        audioEngine.attach(sourceNode)
+        audioEngine.connect(sourceNode, to: mainMixer, format: inputFormat)
+        audioEngine.connect(mainMixer, to: outputNode, format: nil)
+        mainMixer.outputVolume = 0
+        do {
+            try audioEngine.start()
+        } catch {
+            print("Could not start engine: \(error.localizedDescription)")
         }
+    }
     
-    //MARK: Public Funtions
+    //MARK: Public Functions
+    public func setWaveformTo(_ signal: @escaping Signal) {
+        self.signal = signal 
+    }
 }
 
 
